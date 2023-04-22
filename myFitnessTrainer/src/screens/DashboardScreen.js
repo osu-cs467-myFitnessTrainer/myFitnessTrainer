efd11f1f93130f238948f4333a52c8ba46090be5
Elizabeth Thorne <66274014+ethorne2@users.noreply.github.com>
2023-04-21 21:18:36 -0500

    setup signin/up with Firebase Auth, add user to db

---
+++ myFitnessTrainer/src/screens/DashboardScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { auth } from '../../firebaseConfig';
+import SignOutButton from '../components/SignOutButton';
+
+const DashboardScreen = ({navigation}) => {
+
+    // TO DO: SIGN OUT BUTTON TO BE MOVED TO SETTINGS...HERE FOR TESTING
+    return (
+    <View style={styles.container}>
+        <Text>Email: {auth.currentUser?.email}</Text>
+        <Text>This screen is the Dashboard! Woot!</Text>
+        <SignOutButton />
+    </View>
+    )
+}
+
+export default DashboardScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
---
+++ myFitnessTrainer/src/screens/SignUpScreen.js
@@ -0,0 +1,176 @@
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { db, auth } from '../../firebaseConfig';
+import { onAuthStateChanged, createUserWithEmailAndPassword } from 'firebase/auth';
+import { addDoc, collection, doc } from 'firebase/firestore';
+
+const SignUpScreen = ({navigation}) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            if(user) {
+                // TO DO: TAKE USER TO AVATAR SCREEN ONCE CREATED
+                navigation.reset({
+                    index: 0,
+                    routes: [{name:'Dashboard'}]
+                });
+            }
+        });
+
+        return unsubscribe;
+    }, []);
+
+    const addUserToDatabase = () => {
+        try {
+            addDoc(collection(db, 'users'), 
+            {
+                // TO DO: UPDATE TO A DEFAULT IMAGE INSTEAD OF TEST
+                avatar_id: doc(db, 'avatars/testAvatar'),
+                email: email,
+                first_name: firstName,
+                last_name: lastName,
+                username: username
+            })
+            .then(docRef => {
+                console.log("Document written with ID: ", docRef.id);
+            });
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
+    };
+
+    const validateInput = () => {
+        // TO DO: Can enhance validation with an error message under each input field
+        if (firstName === '' || lastName === '' || username === '' || email === '' || password === '') {
+            Alert.alert('Missing Fields', 'All fields are required to create an account.');
+            return false;
+        }
+        return true;
+    };
+
+    const handleSignUp = () => {
+
+        if (!validateInput()) {
+            return;
+        }
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredentials => {
+            const user = userCredentials.user;
+            console.log('Signed up with:', user.email);
+            addUserToDatabase();
+        })
+        .catch(error => {
+            alert(error.message);
+        })
+    };
+
+
+  return (
+    <View
+        style={styles.container}
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+            placeholder='First Name'
+            value={firstName}
+            onChangeText={text => setFirstName(text)}
+            style={styles.input}
+        />
+        <TextInput
+            placeholder='Last Name'
+            value={lastName}
+            onChangeText={text => setLastName(text)}
+            style={styles.input}
+        />
+        <TextInput
+            placeholder='Username'
+            value={username}
+            onChangeText={text => setUsername(text)}
+            style={styles.input}
+            autoCapitalize='none'
+        />
+        <TextInput
+            placeholder='Email'
+            value={email}
+            onChangeText={text => setEmail(text)}
+            style={styles.input}
+            autoCapitalize='none'
+        />
+        <TextInput
+            placeholder='Password'
+            value={password}
+            onChangeText={text => setPassword(text)}
+            style={styles.input}
+            secureTextEntry
+            autoCapitalize='none'
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+            onPress={handleSignUp}
+            style={[styles.button, styles.buttonOutline]}
+        >
+            <Text style={styles.buttonOutlineText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default SignUpScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        width: '80%',
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        borderRadius: 10,
+        marginTop: 15
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40
+    },
+    button: {
+        backgroundColor: '#4682B4',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    signUpMessage: {
+        marginTop: 30,
+        textAlign: 'center'
+    },
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 10,
+        borderColor: '#4682B4',
+        borderWidth: 2
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 800,
+        fontSize: 16
+    },
+    buttonOutlineText: {
+        color: '#4682B4',
+        fontWeight: 800,
+        fontSize: 16
+    },
+});
