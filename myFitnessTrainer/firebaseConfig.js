fb1c5bbaf88098ee83ef04b9e0c25c1983c33653
Jenn <um.jennifer@gmail.com>
2023-04-27 17:22:13 -0700

    test adding change

--- myFitnessTrainer/firebaseConfig.js
+++ myFitnessTrainer/firebaseConfig.js
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 // import {...} from "firebase/functions";
-
+// 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
