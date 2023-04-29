49af49284a5ae5f913941b6dbe6d000fa126f995
kasaechao <saechaok@oregonstate.edu>
2023-04-29 14:14:39 -0700

    initial survey algo

--- myFitnessTrainer/src/components/GenerateNewPlanAlgoButton.js
+++ myFitnessTrainer/src/components/GenerateNewPlanAlgoButton.js
@@ -7,9 +7,7 @@ import {
     query,
     where,
     and,
-    getDoc,
     addDoc,
-    doc,
 } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import { auth } from "../../firebaseConfig";
