9d73bfc76fe9048a45e749a037688bc3b643f9a5
Elizabeth Thorne <66274014+ethorne2@users.noreply.github.com>
2023-04-30 16:35:16 -0500

    move vars to .env

--- dbScript/buildDB.js
+++ dbScript/buildDB.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 // exercise library json file import
 import * as exercise_library from "../exercise_library/exercise_library.json" assert { type: "json" }
 import { addDoc, collection, getFirestore } from "firebase/firestore";
@@ -6,13 +9,13 @@ import { initializeApp } from "firebase/app";
 // TO DO: Add these config ID's to a .env file (remove from this file) 
 // firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyBj1gTTLeBEFxXOpvSVevhoW_68j3_lydU",
-    authDomain: "myfitnesstrainer-94289.firebaseapp.com",
-    projectId: "myfitnesstrainer-94289",
-    storageBucket: "myfitnesstrainer-94289.appspot.com",
-    messagingSenderId: "878461072720",
-    appId: "1:878461072720:web:c50cd2b52e9b6ef3e42855",
-    measurementId: "G-D4REKPXY76",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- myFitnessTrainer/firebaseConfig.js
+++ myFitnessTrainer/firebaseConfig.js
@@ -1,3 +1,12 @@
+import { 
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from "@env";
 import { initializeApp } from 'firebase/app';
 
 
@@ -10,15 +19,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// TO DO: Add these config ID's to a .env file (remove from this file)
 const firebaseConfig = {
-    apiKey: "AIzaSyBj1gTTLeBEFxXOpvSVevhoW_68j3_lydU",
-    authDomain: "myfitnesstrainer-94289.firebaseapp.com",
-    projectId: "myfitnesstrainer-94289",
-    storageBucket: "myfitnesstrainer-94289.appspot.com",
-    messagingSenderId: "878461072720",
-    appId: "1:878461072720:web:c50cd2b52e9b6ef3e42855",
-    measurementId: "G-D4REKPXY76"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
