a1fd04daf55eba6f7ac56373f3cf31bd731c5963
kasaechao <saechaok@oregonstate.edu>
2023-04-30 22:03:08 -0700

    refactor GenerateNewPlanAlgoButton.js by moving the database helper functions to external file

--- myFitnessTrainer/src/components/GenerateNewPlanAlgoButton.js
+++ myFitnessTrainer/src/components/GenerateNewPlanAlgoButton.js
@@ -1,16 +1,10 @@
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import {
-    collection,
-    getDocs,
-    query,
-    where,
-    and,
-    addDoc,
-} from "firebase/firestore";
+import { collection, getDocs, query, where, and } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import { auth } from "../../firebaseConfig";
+import { getDocumentId, postDocument } from "../../databaseFunctions";
 
 /*
     Pass in the following props as the below types:
@@ -19,7 +13,6 @@ import { auth } from "../../firebaseConfig";
     - duration: Int
     - modifications: Array<String>
 */
-
 const GenerateNewPlanAlgoButton = ({ goal, level, duration }) => {
     // Test props for debugging
     goal = goal ?? ["strength"];
@@ -37,15 +30,20 @@ const GenerateNewPlanAlgoButton = ({ goal, level, duration }) => {
     const handleCreateNewPlanAlgo = async () => {
         const retrievedExercises = await getExercises(goal, level);
         const workoutPlan = createPlan(retrievedExercises, duration);
-        const userId = await getUserId(auth.currentUser.email);
-        await postToWorkoutPlans(
+        const userId = await getDocumentId(
+            "users",
+            "email",
+            auth.currentUser.email
+        );
+
+        await postDocument("workout_plans", {
             workoutPlan,
             duration,
             goal,
             level,
             userId,
-            db
-        );
+        });
+
         navigation.replace("Dashboard");
     };
 
@@ -76,8 +74,6 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
 });
-
-// HELPER FUNCTIONS
 
 /*
     performs query to db for exercises collection based on goal and level survey result and returns an array of exercises
@@ -130,37 +126,4 @@ const filterQuery = (goal, level) => {
         default:
             break;
     }
-};
-
-const postToWorkoutPlans = async (
-    dailyExercises,
-    duration,
-    goal,
-    level,
-    userId,
-    db
-) => {
-    const postObject = {
-        active: "",
-        duration_in_days: duration,
-        fitness_goal: goal,
-        fitness_level: level,
-        workout_modification_id: "",
-        user_id: userId,
-        daily_exercises: dailyExercises,
-    };
-
-    const postRef = await addDoc(collection(db, "workout_plans"), postObject);
-    return postRef;
-};
-
-
-const getUserId = async (userEmail) => {
-    let userId;
-    const q = query(collection(db, "users"), where("email", "==", userEmail));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        userId = doc.id;
-    });
-    return userId;
 };
