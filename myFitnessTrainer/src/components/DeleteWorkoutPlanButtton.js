e37189c181bd3bc903312e75cdfb0db2596947c5
Jenn <um.jennifer@gmail.com>
2023-05-13 21:56:39 -0700

    Successfully deleted workout plan

--- myFitnessTrainer/src/components/DeleteWorkoutPlanButtton.js
+++ myFitnessTrainer/src/components/DeleteWorkoutPlanButtton.js
@@ -1,14 +1,37 @@
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { db, auth } from "../../firebaseConfig";
+import { getDocumentId  } from "../../databaseFunctions";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 // https://firebase.google.com/docs/firestore/query-data/queries#compound_and_queries
 
 const DeleteWorkoutPlanButton = () => {
     const navigation = useNavigation();
 
-    const handleDeleteWorkoutPlan = () => {
-        navigation.navigate("Delete Workout Plan");
+    const handleDeleteWorkoutPlan = async () => {
+        // get curr, active plan
+        const userId = await getDocumentId(
+            "users",
+            "email",
+            auth.currentUser.email
+        );
+        const workoutPlanRef = collection(db, "workout_plans");
+        const q = query(workoutPlanRef, where("user_id", "==", userId), where("active", "==", true));
+        const querySnapshot = await getDocs(q);
+        let hasActiveWorkoutPlan = false;
+        let workoutPlanId = null;
+        querySnapshot.forEach((doc) => {
+            workoutPlanId = doc.id;
+            hasActiveWorkoutPlan = doc.data()["active"];
+            // TODO: send metrics about existing current active plan 
+        });
+        // console.log("hasActiveWorkoutPlan=", hasActiveWorkoutPlan);
+        navigation.navigate("Delete Workout Plan", {
+            hasActiveWorkoutPlan: hasActiveWorkoutPlan,
+            workoutPlanId: workoutPlanId
+          });
     };
 
     return (
--- myFitnessTrainer/src/screens/DeleteWorkoutPlanScreen.js
+++ myFitnessTrainer/src/screens/DeleteWorkoutPlanScreen.js
@@ -1,16 +1,57 @@
 import React from "react";
-import { StyleSheet, View, Text, ScrollView} from "react-native";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Alert} from "react-native";
 import CreateNewPlanButton from "../components/CreateNewPlanButton";
-
-// TODO: in generatenewplanalgo, create deleted field or just delete document
-
+import { useNavigation } from "@react-navigation/native";
+import { db } from "../../firebaseConfig";
+import { doc, deleteDoc } from "firebase/firestore";
 
 
 const DeleteWorkoutPlanScreen = ({route}) => {
+    const navigation = useNavigation();
+
+    const deleteWorkoutPlanInDBAndGoToDashboard = async () => {
+        workoutPlanId = route.params.workoutPlanId;
+        // console.log("workoutPlanId to delete=", workoutPlanId);
+        await deleteDoc(doc(db, "workout_plans", workoutPlanId));
+        // TODO: delete exercise_history where workoutPlanId is referenced
+        navigation.navigate("Dashboard");
+    }
+
+    const handleConfirmDeleteWorkoutPlan = () => {
+        Alert.alert(
+            "Are you sure you want to delete this workout plan?",
+            "This action cannot be undone.",
+            [
+                { text: "Cancel", style: 'cancel', isPreferred: true, onPress: ()=> {}},
+                {
+                    text: 'Delete',
+                    style: 'destructive',
+                    onPress: deleteWorkoutPlanInDBAndGoToDashboard,
+                },
+            ]
+        );
+    };
+
+
+    // console.log("route.params.hasActiveWorkoutPlan=", route.params.hasActiveWorkoutPlan);
+    if (!route.params.hasActiveWorkoutPlan){
+        return(
+            <ScrollView>
+            <View style={styles.container}>
+                <Text>You currently have no active workout plans.</Text>
+                <CreateNewPlanButton/>
+            </View>
+            </ScrollView>
+        )   
+    } 
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <Text>delete workout screen</Text>
+                <Text>TODO: stuff about the current active plan here</Text>
+                <TouchableOpacity style={styles.button} onPress={handleConfirmDeleteWorkoutPlan}>
+                    <Text style={styles.buttonText}>Delete Workout Plan</Text>
+                </TouchableOpacity>
             </View>
         </ScrollView>
     );
@@ -21,7 +62,20 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         paddingBottom: 40
-    }
+    },
+    button: {
+        backgroundColor: '#8B0000',
+        width: '75%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 800,
+        fontSize: 16
+    },
 });
 
 export default DeleteWorkoutPlanScreen;
